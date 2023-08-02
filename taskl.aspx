cb198d14cf5b00686d4d01bec42d6860712efe3c
harsimrangill-dev <125225536+harsimrangill-dev@users.noreply.github.com>
2023-08-02 01:34:02 +0100

    Contains all the code and solution of app

---
+++ taskl.aspx
@@ -0,0 +1,69 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="taskl.aspx.cs" Inherits="taskl" %>
+<link href="layout.css" rel="stylesheet" />
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div class="hed" >
+            <b>Tasks To Do</b>
+        </div>
+        <div class="tbl"  >
+            <table>
+            </table>
+            <asp:Table CssClass="timecard" runat="server" ID="tbl">
+
+                <asp:TableHeaderRow>
+                    <asp:TableHeaderCell>Serial No.</asp:TableHeaderCell>
+                    <asp:TableHeaderCell>
+                        Tasks
+                    </asp:TableHeaderCell>
+                    <asp:TableHeaderCell>Progress</asp:TableHeaderCell>
+                </asp:TableHeaderRow>
+            </asp:Table>
+            <br />
+            
+        </div>
+        
+        <div class="task">
+            <table>
+                <tr>
+                    <td>Task :</td>
+                    <td> <asp:TextBox ID="taskinput" runat="server"></asp:TextBox></td>
+                </tr>
+                <tr>
+                    <td><asp:Button ID="Add" runat="server" OnClick="adds" Text="Add" /></td>
+                </tr>
+            </table>
+            <br /><br />
+            <table>
+                <tr>
+                    <td>Serial no.</td>
+                    <td><asp:TextBox ID="Update" runat="server"></asp:TextBox></td>
+                </tr>
+                <tr>
+                    <td>
+                        <asp:Button ID="updt" runat="server" OnClick="updte" Text="Update" />
+                    </td>
+                </tr>
+            </table>
+            <br /><br />
+            <table>
+                <tr>
+                    <td>Serial No. </td>
+                    <td><asp:TextBox ID="deltask" runat="server"></asp:TextBox></td>
+                </tr>
+                <tr>
+                    <td><asp:Button ID="del" runat="server" OnClick="delt" Text="DELETE" /></td>
+                </tr>
+            </table>
+        </div>
+        <div class="foot">
+            <asp:Label ID="lbl" runat="server"></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ taskl.aspx.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class taskl : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        lbl.Text = string.Empty;
+        if (!IsPostBack)
+        {
+            crt();
+        }
+    }
+
+    private bool isexist(string name, string source)
+    {
+        SqlConnection conn = new SqlConnection(source);
+        conn.Open();
+        DataTable dt = conn.GetSchema("TABLES", new string [] {null, null, name });
+        conn.Close();
+        return dt.Rows.Count> 0;
+    }
+
+    private void crt()
+    {
+        
+        string cont = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Computing\source\repos\WebSite1\App_Data\Database.mdf;Integrated Security=SSPI";
+        SqlConnection con = new SqlConnection(cont);
+        SqlCommand cmd = new SqlCommand();
+        SqlDataReader rdr;
+
+        try
+        {
+            string create_tbl = "CREATE TABLE task (task varchar(255), progress varchar(255))";
+            string update = "ALTER TABLE task DROP COLUMN id; ALTER TABLE task ADD  id  INT IDENTITY(1,1)";
+            //update = "UPDATE task SET id= INT IDENTITY(1,1)";
+            string gets = "SELECT  * FROM task";
+            int val = 0;
+            cmd.Connection= con;
+            int add;
+            con.Open();
+            if (isexist("task", cont))
+            {
+                //
+
+            }
+            else
+            {
+                val++;
+                cmd.CommandText = create_tbl;
+                add = cmd.ExecuteNonQuery();
+            }
+            cmd.CommandText = update;
+            cmd.ExecuteNonQuery();
+            cmd.CommandText = gets;
+            rdr = cmd.ExecuteReader();
+
+            while (rdr.Read())
+            {
+                
+
+                TableRow row = new TableRow();
+                TableCell cell1 = new TableCell();
+                TableCell cell2= new TableCell();
+                TableCell cell3 = new TableCell();
+                cell1.Text = rdr[3].ToString();
+                cell2.Text = rdr[1].ToString();
+                cell3.Text= rdr[2].ToString();
+                row.Controls.Add(cell1);
+                row.Controls.Add(cell2);
+                row.Controls.Add(cell3);
+                tbl.Controls.Add(row);
+            }
+
+        }
+        catch
+        {
+            lbl.Text = "Sorry the excution had not done";
+        }
+
+        finally
+        {
+            con.Close();
+            
+        }
+    }
+
+    protected void delt(object o, EventArgs e)
+    {
+        string cont = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Computing\source\repos\WebSite1\App_Data\Database.mdf;Integrated Security=SSPI";
+        SqlConnection con = new SqlConnection(cont);
+        SqlCommand cmd = new SqlCommand();
+        cmd.Connection = con;
+        int attemt = 0;
+        try
+        {
+            string command = "DELETE FROM task WHERE id=@num";
+            cmd.CommandText = command;
+            cmd.Parameters.AddWithValue("@num", deltask.Text);
+            con.Open();
+            attemt = cmd.ExecuteNonQuery();
+        }
+        catch
+        {
+            lbl.Text = "Command is not exceuted";
+        }
+        finally { con.Close(); }
+
+        crt();
+        
+    }
+
+    protected void adds(object o, EventArgs e)
+    {
+        string cont = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Computing\source\repos\WebSite1\App_Data\Database.mdf;Integrated Security=SSPI";
+        SqlConnection con = new SqlConnection(cont);
+        SqlCommand cmd = new SqlCommand();
+        cmd.Connection = con;
+        int attemt = 0;
+        try
+
+        {
+
+            string command = "INSERT INTO task(task,progress) VALUES(@tsk,@prg)";
+            cmd.CommandText = command;
+            cmd.Parameters.AddWithValue("@tsk", taskinput.Text);
+            cmd.Parameters.AddWithValue("@prg",  "Not Done");
+            con.Open();
+            attemt = cmd.ExecuteNonQuery();
+        }
+        catch
+        {
+            lbl.Text = "Command is not exceuted";
+        }
+        finally { con.Close(); }
+
+
+            crt();
+
+    }
+
+    protected void updte(object o, EventArgs e)
+    {
+        string cont = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Computing\source\repos\WebSite1\App_Data\Database.mdf;Integrated Security=SSPI";
+        SqlConnection con = new SqlConnection(cont);
+        SqlCommand cmd = new SqlCommand();
+        cmd.Connection = con;
+        int attemt = 0;
+        try
+        {
+            string command = "UPDATE task SET progress=@prg WHERE  id=@num";
+            cmd.CommandText = command;
+            cmd.Parameters.AddWithValue("@num", Update.Text);
+            cmd.Parameters.AddWithValue("@prg", "Done");
+            con.Open();
+            attemt = cmd.ExecuteNonQuery();
+        }
+        catch
+        {
+            lbl.Text = "Command is not exceuted";
+        }
+        finally { con.Close(); }
+
+
+            crt();
+
+    }
+
+}
